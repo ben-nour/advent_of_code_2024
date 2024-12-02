1f55d73350637ad3e90ff70baa32128e6e683682
Benjamin Nour <ben.nour@live.com.au>
2024-12-02 14:02:35 +1100

    updated day 1 code

--- scripts/day_one.py
+++ scripts/day_one.py
@@ -1,6 +1,5 @@
-# Day one.
-
 # Part 1.
+
 first_list = []
 second_list = []
 with open("./data/day_one_input") as file:
@@ -9,10 +8,12 @@ with open("./data/day_one_input") as file:
         first_list.append(int(line[0]))
         second_list.append(int(line[1]))
 
-differences = []
-for first_number, second_number in zip(sorted(first_list), sorted(second_list)):
-    differences.append(abs(first_number - second_number))
-answer = sum(differences)
+answer = sum(
+    [
+        abs(first_number - second_number)
+        for first_number, second_number in zip(sorted(first_list), sorted(second_list))
+    ]
+)
 print(answer)
 
 # Part 2.

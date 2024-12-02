93e66b14afaa89d00ff1c894a41ef4ee984d16ee
Benjamin Nour <ben.nour@live.com.au>
2024-12-03 10:19:25 +1100

    update

--- scripts/day_two.py
+++ scripts/day_two.py
@@ -1,1 +1,74 @@
 # Part 1.
+
+from copy import copy
+
+with open("./data/day_two_input.txt") as file:
+    levels = [line.split() for line in file]
+
+LEVELS_COUNT = len(levels)
+
+
+def process_levels(levels):
+    unsafe_levels = 0
+    for level in levels:
+        for index, number in enumerate(level):
+            number = int(number)
+            previous_number = int(level[index - 1])
+            if index == 1:
+                if number - previous_number < 0:
+                    flag = 0
+                else:
+                    flag = 1
+            if index != 0:
+                if number - previous_number > 3:
+                    unsafe_levels += 1
+                    break
+                if number < previous_number and abs(number - previous_number) > 3:
+                    unsafe_levels += 1
+                    break
+                if number == previous_number:
+                    unsafe_levels += 1
+                    break
+                if number - previous_number < 0:
+                    new_flag = 0
+                else:
+                    new_flag = 1
+                if new_flag != flag:
+                    unsafe_levels += 1
+                    break
+
+    return unsafe_levels
+
+
+answer = LEVELS_COUNT - process_levels(levels)
+print(answer)
+
+
+# Part 2.
+def create_list_versions(original: list) -> list:
+    """
+    Create different versions of a list. Each version has one element removed.
+    """
+    copies = []
+    for index, number in enumerate(original):
+        list_copy = copy(original)
+        del list_copy[index]
+        copies.append(list_copy)
+    copies.insert(0, original)
+    return copies
+
+
+"""
+invalid_levels = 0
+for level in levels:
+    different_versions = create_list_versions(level)
+    level_version_fault_count = 0
+    for version in different_versions:
+
+
+    if level_version_fault_count > 1:
+        invalid_levels += 1
+
+answer = LEVELS_COUNT - invalid_levels
+print(answer)
+"""
